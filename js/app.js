8d761d37023016a0b99285e92f3cfb1918fced5e
Victor <viclee02@gmail.com>
2018-12-09 22:45:50 -1000

    project init

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//1. Target the h3 element with the id of 'disclaimer' and change the contents to the follow:
+
+// Coupons VALID from Tuesday 12/4/2018 thru Saturday 6/29/2019. All sales FINAL. NO REFUNDS and EXCHANGES. And definitely NO RAINCHECKS! And ONLY POSITIVES reviews allowed on Social Media (FB, Instagram, Twitter, Yelp, etc.). And PLEASE Follow us on FB, Instagram, Twitter.
+
+// disclaimer.innerHTML = 'Coupons VALID from Tuesday 12/4/2018 thru Saturday 6/29/2019. All sales FINAL. NO REFUNDS and EXCHANGES. And definitely NO RAINCHECKS! And ONLY POSITIVES reviews allowed on Social Media (FB, Instagram, Twitter, Yelp, etc.). And PLEASE Follow us on FB, Instagram, Twitter.'
