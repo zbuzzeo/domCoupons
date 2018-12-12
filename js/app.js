41a13ff8a95dda97c0797b0b3540d8baf40d0f84
madycakes <viclee02@gmail.com>
2018-12-11 14:39:40 -1000

    adds ex 1 - 5 questions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 //1. Target the h3 element with the id of 'disclaimer' and change the contents to the follow:
 
-// Coupons VALID from Tuesday 12/4/2018 thru Saturday 6/29/2019. All sales FINAL. NO REFUNDS and EXCHANGES. And definitely NO RAINCHECKS! And ONLY POSITIVES reviews allowed on Social Media (FB, Instagram, Twitter, Yelp, etc.). And PLEASE Follow us on FB, Instagram, Twitter.
+var finePrint = 'Coupons VALID from Tuesday 12/4/2018 thru Saturday 6/29/2019. All sales FINAL. NO REFUNDS and EXCHANGES. And definitely NO RAINCHECKS! And ONLY POSITIVE reviews allowed on Social Media (FB, Instagram, Twitter, Yelp, etc.). And PLEASE Follow us on FB, Instagram, Twitter.'
 
-// disclaimer.innerHTML = 'Coupons VALID from Tuesday 12/4/2018 thru Saturday 6/29/2019. All sales FINAL. NO REFUNDS and EXCHANGES. And definitely NO RAINCHECKS! And ONLY POSITIVES reviews allowed on Social Media (FB, Instagram, Twitter, Yelp, etc.). And PLEASE Follow us on FB, Instagram, Twitter.'
+disclaimer.innerHTML = finePrint;
+
+// 2. Target the div elemenet with the id of 'brand1' and update the content to Nabisco.
+
+brand1.innerHTML = 'Nabisco';
+
+// 3. Target the div element with the id of 'item1' and update the content with Cheeseburger Oreos. 
+
+item1.innerHTML = 'Cheeseburger Oreos';
+
+// 4. Target the div element with the id of 'price1' and update the content to $8.99.
+
+price1.innerHTML = '$8.99';
+
+// 5. Target the button element with the id of 'discount1' and update the content to Free fries with purchase!
+
+discount1.innerHTML = 'Free fries with purchase!'
