fb83eedab5b63b0a0370f2bb39a74cb97a9798d9
madycakes <viclee02@gmail.com>
2018-12-13 16:05:11 -1000

    minor edits on spacing

--- js/app.js
+++ js/app.js
@@ -4,33 +4,33 @@
 
 //1. Target the h3 element with the id of 'disclaimer' and change the contents to the follow:
 
-// 2. Target the div elemenet with the id of 'brand1' and update the content to Nabisco.
+//2. Target the div elemenet with the id of 'brand1' and update the content to Nabisco.
 
-// 3. Target the div element with the id of 'item1' and update the content with Cheeseburger Oreos. 
+//3. Target the div element with the id of 'item1' and update the content with Cheeseburger Oreos. 
 
-// 4. Target the div element with the id of 'price1' and update the content to $8.99.
+//4. Target the div element with the id of 'price1' and update the content to $8.99.
 
-// 5. Target the button element with the id of 'discount1' and update the content to Free fries with purchase!
+//5. Target the button element with the id of 'discount1' and update the content to Free fries with purchase!
 
-// 6. Create a div element with the id of 'item2' and update the content to Hendrick's Gin. Append this element to William Grand & Sons div.
+//6. Create a div element with the id of 'item2' and update the content to Hendrick's Gin. Append this element to William Grand & Sons div.
 
-// 7. Create a div element with the id of 'price2' and update the content to $34.99. Append this element to the Hendrick's Gin div.
+//7. Create a div element with the id of 'price2' and update the content to $34.99. Append this element to the Hendrick's Gin div.
 
-// 8. Create a button element with the id of 'discount3' and give it a content of 50% OFF Axe body soap!. Append this element to the appropriate div class 'block3.
+//8. Create a button element with the id of 'discount3' and give it a content of 50% OFF Axe body soap!. Append this element to the appropriate div class 'block3.
 
-// 9. Target the div element with a class of 'item' for the Panda Express section and update the content to Orange Chicken.
+//9. Target the div element with a class of 'item' for the Panda Express section and update the content to Orange Chicken.
 
-// 10. Create a button element with a class of 'discount' and update the content to Free T-shirt and fortune cookie with purchase! Append this button to Panda Express section.
+//10. Create a button element with a class of 'discount' and update the content to Free T-shirt and fortune cookie with purchase! Append this button to Panda Express section.
 
-// 11. Target the div element with the class of 'brand' for the red hoodie section and update the content to Uniqlo.
+//11. Target the div element with the class of 'brand' for the red hoodie section and update the content to Uniqlo.
 
-// 12. Target the div element with the class of 'price' for the red hoodie section and update the content to 10,000. 
+//12. Target the div element with the class of 'price' for the red hoodie section and update the content to 10,000. 
 
-// 13. Create a div element with the class of 'brand' and give it a content of Hifiman Shangri-La. Append this element in the headphones section. 
+//13. Create a div element with the class of 'brand' and give it a content of Hifiman Shangri-La. Append this element in the headphones section. 
 
-// 14. Create a div element with the class of 'item' and give it a content of Electronstatic Headphones. Append this element in the headphones section. 
+//14. Create a div element with the class of 'item' and give it a content of Electronstatic Headphones. Append this element in the headphones section. 
 
-// 15. Create a div element with the class of 'price' and give it a content of $18,000. Append this element in the headphones section.
+//15. Create a div element with the class of 'price' and give it a content of $18,000. Append this element in the headphones section.
 
 //16. Create a button element with the class of 'discount' and give it a content of Free Barry Manilow CD with purchase! Append this element in the headphones section.
 
